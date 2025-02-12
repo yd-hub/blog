b850fa8fde5ce6595cc8a8ee00c8c75824c2f1d9
zhao <583833826@qq.com>
2025-02-12 13:55:14 +0800

    修改字体

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -10,7 +10,11 @@ export default defineUserConfig({
   head: [
     ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],    
     ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: ""}],
-    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap' }],
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap' }],  // Lora
+    ["link",{ href: "https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@400;500;700&display=swap",rel: "stylesheet" }], // 思源宋体
+    ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css' }],  // 楷体
+
+    
   ],
   theme,
 
