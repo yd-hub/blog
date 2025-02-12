76a8fc9cc3af062eda889c92d14a3eced32f98f9
zhao <583833826@qq.com>
2025-02-12 13:19:19 +0800

    添加字体

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -8,8 +8,11 @@ export default defineUserConfig({
   title: "zhao's blog",
   description: "zhao's blog",
   head: [
-    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Dancing+Script&family=Lora&display=swap' }]
+    ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],    
+    ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: ""}],
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&family=Lora:ital,wght@0,400..700;1,400..700&display=swap' }],
   ],
+
   theme,
 
   // 和 PWA 一起启用
