ced9fe09d07a844cfd2d6a41135d1c11e068451b
zhao <583833826@qq.com>
2025-02-12 13:38:10 +0800

    修改字体

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -10,9 +10,8 @@ export default defineUserConfig({
   head: [
     ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],    
     ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: ""}],
-    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&family=Lora:ital,wght@0,400..700;1,400..700&display=swap' }],
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap' }],
   ],
-
   theme,
 
   // 和 PWA 一起启用
