388d32b4125803308ec98b95c9f6e5107b346bd3
zhao <583833826@qq.com>
2025-02-14 23:59:08 +0800

    测试提交

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,4 +1,5 @@
 import { defineClientConfig } from "vuepress/client";
+import { defineGiscusConfig } from '@vuepress/plugin-comment/client'
 import imageProxy from "./components/imageProxy.vue";
 // navbar 透明
 import { setupTransparentNavbar } from "vuepress-theme-hope/presets/transparentNavbar.js";
@@ -11,3 +12,12 @@ export default defineClientConfig({
     app.component("imageProxy", imageProxy);
   },
 });
+
+defineGiscusConfig({
+  // Giscus 选项
+  repo: "https://github.com/your-username/your-repo",
+  repoId: "MDEwOlJlcG9zaXRvcnkxMTYyMzQ5NzQ=",
+  category: "Announcements",
+  categoryId: "DIC_kwDOFEEY",
+  mapping: "pathname",
+})
